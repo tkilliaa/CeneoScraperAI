9567ceff4d0c714370099b5e2ad1c5e8e123bed3
“tkillia” <s218646@student.uek.krakow.pl>
2021-05-13 17:55:49 +0300

    2021-05-13 IT

--- app/models/opinion.py
+++ app/models/opinion.py
@@ -1,54 +1,60 @@
 from app import app
-from app.utils import extractElements
+from app.utils import extractElement
+
 class Opinion:
 
-        selectors = {
-            "author": ["span.user-post__author-name"],
-            "recomendation": ["span.user-post__author-recomendation > em"],
-            "stars": ["span.user-post__score-count"],
-            "content": ["div.user-post__text"],
-            "advantages": ["div.review-feature__col:has(> div[class*=\"positives\"])"],
-            "disadvantages": ["div.review-feature__col:has(> div[class*=\"negatives\"])"],
-            "helpful": ["button.vote-yes > span"],
-            "unhelpful": ["button.vote-no > span"],
-            "publishDate": ["span.user-post__published > time:nth-child(1)","datetime"],
-            "purchaseDate": ["span.user-post__published > time:nth-child(2)","datetime"]
-        }
+    selectors = {
+        "author": ["span.user-post__author-name"],
+        "recommendation": ["span.user-post__author-recomendation > em"],
+        "stars": ["span.user-post__score-count"],
+        "content": ["div.user-post__text"],
+        "advantages": ["div.review-feature__col:has(> div[class*=\"positives\"])"],
+        "disadvantages": ["div.review-feature__col:has(> div[class*=\"negatives\"])"],
+        "helpful": ["button.vote-yes > span"],
+        "unhelpful": ["button.vote-no > span"],
+        "publishDate": ["span.user-post__published > time:nth-child(1)", "datetime"],
+        "purchaseDate": ["span.user-post__published > time:nth-child(2)", "datetime"]
+    }
 
-        def __init__(self, opinionID=None, author=None, recomendation=None, stars=None, content=None, advantages=None, disadvantages=None, helpful=None, unhelpful=None, purchased=None, purchaseDate=None, publishDate=None):
-            self.opinionID = opinionID
-            self.author = author
-            self.recomendation = recomendation
-            self.stars = stars
-            self.content = content
-            self.advantages = advantages
-            self.disadvantages = disadvantages
-            self.helpful = helpful
-            self.unhelpful = unhelpful
-            self.purchased = purchased
-            self.purchaseDate = purchaseDate
-            self.publishDate = publishDate
+    def __init__(self, opinionId=None, author=None, recommendation=None, stars=None, content=None, advantages=None, disadvantages=None,
+                 helpful=None, unhelpful=None, purchased=None, purchaseDate=None, publishDate=None):
+        self.opinionId = opinionId
+        self.author = author
+        self.recommendation = recommendation
+        self.stars = stars
+        self.content = content
+        self.advantages = advantages
+        self.disadvantages = disadvantages
+        self.helpful = helpful
+        self.unhelpful = unhelpful
+        self.purchased = purchased
+        self.purchaseDate = purchaseDate
+        self.publishDate = publishDate
+    
+    def extractOpinion(self,opinionTree):
+        for key, value in self.selectors.items():
+            setattr(self, key, extractElement(opinionTree, *value))
+        self.opinionId = opinionTree["data-entry-id"]
+        self.transformOpinion()
 
-        def extractOpinion(self, opinionTree):
-            for key, value in self.selectors.items():
-                setattr(self, key, extractElement(opinion, *value))
-            self.opinionID = opinion["data-entry-id"]
-            self.transformOpinioin()
-        def transformOpinioin(self):
-            try:
-                    self.advantages = self.advantages.replace("Zalety\n", "").replace("\n", ", ")
-                except AttributeError:
-                    pass
-                try:
-                    self.disadvantages = self.disadvantages.replace("Wady\n", "").replace("\n", ", ")
-                except AttributeError:
-                    pass
-                
-                self.recomendation = True if self.recomendation == "Polecam" else False if self.recomendation == "Nie Polecam" else None
-                self.stars = float(self.stars.split("/")[0].replace(",","."))
-                self.content = self.content.replace("\n", " ").replace("\r", " ").replace("\t", " ")
-                self.helpful = int(self.helpful)
-                self.unhelpful = int(self.unhelpful)
-
-        def __str__(self): #return the product and details
+    def transformOpinion(self):
+        try:
+            self.advantages = self.advantages.replace("Zalety\n", "").replace("\n", ", ")
+        except AttributeError:
             pass
+        try:
+            self.disadvantages = self.disadvantages.replace("Wady\n", "").replace("\n", ", ")
+        except AttributeError:
+            pass
+        self.recommendation = True if self.recommendation == "Polecam" else False if self.recommendation == "Nie polecam" else None
+        self.stars = float(self.stars.split("/")[0].replace(",", "."))
+        self.content = self.content.replace("\n", " ").replace("\r", " ").replace("\t", " ")
+        self.helpful = int(self.helpful)
+        self.unhelpful = int(self.unhelpful)
+
+    def __str__(self):
+        return 'opinionId: '+str(self.opinionId)+'<br>'+'<br>'.join(key+": "+str(getattr(self, key)) for key in self.selectors.keys())
+
+    def __dict__(self):
+        return {'opinionId': self.opinionId}.update({key: getattr(self, key)
+                                                     for key in self.selectors.keys()})
--- app/unitls.py
+++
@@ -1,8 +0,0 @@
-def extractElement(ancestor, selector, attribute=None):
-    try:
-        if attribute:
-            return opinion.select(selector).pop(0)[attribute].strip()
-        else: 
-            return opinion.select(selector).pop(0).text.strip()
-    except IndexError:
-        return None
